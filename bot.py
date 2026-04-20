720942035796963395152b8c3b7e4c896f1008d8
farruxbekboltayev <63141800+farruxbekboltayev@users.noreply.github.com>
2026-04-19 20:21:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -123,12 +123,16 @@ async def check_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # kino yuborish
 async def send_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    if user_id not in users:
-    users.add(user_id)
+    if not update.message:
+        return
 
-    first_name = update.effective_user.first_name
-    last_name = update.effective_user.last_name
-    username = update.effective_user.username
+    user_id = update.effective_user.id
+    if user_id not in users:
+       users.add(user_id)
+
+        first_name = update.effective_user.first_name
+        last_name = update.effective_user.last_name
+        username = update.effective_user.username
 
     if username:
         username_text = f"@{username}"
